48f67714ed700d238a1fe52c7e097ba67e7058b3
vprjct <cacomaple@gmail.com>
2019-09-27 02:04:45 +0900

    Create on_code.rb

---
+++ 11. Container With Most Water/on_code.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} height
+# @return {Integer}
+def max_area(height)
+    # 自分より大きい値で距離が遠いもの
+    size = height.size
+    max = 0
+    l = 0
+    r = size-1
+    while l < r
+        len = [height[l], height[r]].min * (r - l)
+        max = [max, len].max
+        if height[l] < height[r]
+            l += 1
+        else
+            r -= 1
+        end
+    end
+    max
+end
